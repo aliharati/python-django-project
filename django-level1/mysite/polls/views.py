90db53f2ec78e0df098392dfd81b4a29e6624339
Ali Harati <77158617+aliharati@users.noreply.github.com>
2024-03-26 22:38:17 +0330

    Add index.html for polls to templates

--- django-level1/mysite/polls/views.py
+++ django-level1/mysite/polls/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("You are at the polls index")
+    my_dict = {'insert_here': "this is the inserted sentence from first app"}
+    return render(request, "polls/index.html",context=my_dict)
