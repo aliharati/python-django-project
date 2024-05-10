3b77884b4a34764a93001242c1eef6c2f566b0a1
Ali Harati <77158617+aliharati@users.noreply.github.com>
2024-05-10 22:53:40 +0330

    Update polls in django-poll

--- django-poll/mysite/mysite/urls.py
+++ django-poll/mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""bale URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from first_app import views
+from django.urls import include,path
+
+
 urlpatterns = [
-    path('', views.index),
+    path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
-    
 ]
